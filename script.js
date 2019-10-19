b5c0e22598599b3c27c5a6dd246b4ea0b07797e4
Fernando Prado <50056409+fernandoprado1@users.noreply.github.com>
2019-10-19 01:39:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('in javascript file');
+
+function changeImage() {
+
+    console.log("hello");
+    if (document.getElementById("img").src == 'Img\\CFG19 911.png') 
+    {
+        document.getElementById("img").src = 'Img\\CFG19 911.png';
+    }
+    else 
+    {
+        document.getElementById("img").src = 'Img\\CFG19 Outside.jpg';
+        document.getElementById("img2").src = 'Img\\CFG19 Inside.jpg';
+    }
+}
