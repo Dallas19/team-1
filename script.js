b7d66992b7755f130675904d523446e1d7279ee9
Daniel Ramsey <dlramseyjr@gmail.com>
2019-10-18 20:40:48 -0700

    This is new framework for project

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
 
-newPage = function() {
-    // First bind task event to current page div
-    // bindTaskEvents()
-    // Second delete current section
-    // deleteCurrentPage()
-    // Add new section
-    console.log("hi")
+option1 = function() {
+    div1 = document.getElementById("square-1")
+    div2= document.getElementById("square-2")
+
+    if (div1.className === "personal") {
+        div1.className = "outside"
+        div2.className = "inside"
+    } else if (div1.className === "outside") {
+        div1.className = "play"
+        div2.className = "restroom"
+    }
+
 };
 
-deleteCurrentPage = function() {
-    
-}
+option2 = function() {
+    div1 = document.getElementById("square-1")
+    div2 = document.getElementById("square-2")
+    console.log(div1, div2)
+    if (div2.className === "emergency") {
+        div1.className = "now"
+        div2.className = "immenint"
+    } else if (div2.className === "inside") {
+        div1.className = "foodAndWater"
+        div2.className = "sleep"
+    }
+
+};
