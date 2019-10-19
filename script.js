c585b32e9272406251a7f76c7300b1bc8ed853d2
Fernando Prado <50056409+fernandoprado1@users.noreply.github.com>
2019-10-19 03:24:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-
 function changeImage() {
 
-    console.log("hello");
-    if (document.getElementById("img").src == 'Img\\CFG19 911.png') 
-    {
-        document.getElementById("img").src = 'Img\\CFG19 911.png';
-    }
-    else 
+    if (document.getElementById("img").src == 'file:///Users/fernandoprado/Desktop/team-1/img/CFG19%20Leisure%20.png') 
     {
         document.getElementById("img").src = 'Img\\CFG19 Outside.jpg';
         document.getElementById("img2").src = 'Img\\CFG19 Inside.jpg';
+    }
+    else 
+    {
+        document.getElementById("img").src == 'file:///Users/fernandoprado/Desktop/team-1/img/CFG19%20Leisure%20.png'         
     }
 }
