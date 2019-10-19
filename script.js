f5ff1840c227bdf01efc7f3a50252260f36a1040
Shawn George <shawng3307@gmail.com>
2019-10-19 00:46:20 -0500

    Change pictures on click

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('in javascript file');
+var images = [{src: "/Img/sleep.jpg"}, {src: "/Img/play.jpg"}];
+function changeImage() {
+
+    console.log("hello");
+    if (document.getElementById("img").src == 'Img\\CFG19 Alarm .jpg') 
+    {
+        document.getElementById("img").src = 'Img\\CFG19 911.png';
+    }
+    else 
+    {
+        document.getElementById("img").src = 'Img\\CFG19 Alarm .jpg';
+    }
+}
