ad7532333ff5364beb78cee815a8f59abeae6fbf
Shawn George <shawng3307@gmail.com>
2019-10-19 03:39:41 -0500

    working change Image

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 function changeImage() {
-    console.log(document.getElementById("img").src);
-    console.log("hello"); 
     var str = document.getElementById("img").src;
     var lastFive = str.substr(str.length - 7); 
-    if (lastFive === "911.png") 
+
+    if (lastFive == "911.png") 
     {
-        console.log("in if");
         document.getElementById("img").src = 'Img\\CFG19 Alarm .jpg';
     }
     else 
     {
-        console.log("in else");
         document.getElementById("img").src = 'Img\\CFG19 911.png';
     }
 
-    console.log(document.getElementById("img").src);
 }
